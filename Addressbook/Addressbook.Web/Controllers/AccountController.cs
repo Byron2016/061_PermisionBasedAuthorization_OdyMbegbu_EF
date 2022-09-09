5073bd92bf5d800489243af7fc4b2d704853251d
Byron <bgva2005@yahoo.com>
2022-09-09 15:44:03 -0500

    feat: V015 modify some methods from accountcontroller

--- Addressbook/Addressbook.Web/Controllers/AccountController.cs
+++ Addressbook/Addressbook.Web/Controllers/AccountController.cs
@@ -1,4 +1,8 @@
-﻿using Addressbook.Web.Models;
+﻿using Addressbook.Core.Interface.Managers;
+using Addressbook.Core.Models;
+//using Addressbook.Infrastructure.Entities;
+using Addressbook.Web.Models;
+using Addressbook.Web.Utils;
 using Microsoft.AspNet.Identity;
 using Microsoft.Owin.Security;
 using System;
@@ -12,8 +16,15 @@ namespace Addressbook.Web.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<User, int> _user;
+
         //Give me the owin authentication context
         public IAuthenticationManager Authentication => HttpContext.GetOwinContext().Authentication; //new CSharp sintax.
+
+        public AccountController(IAccountManager account)
+        {
+            _user = new UserManager<User, int>(new UserStore(account)); //v15 5.17
+        }
 
         // GET: Login
         public ActionResult Login()
@@ -24,15 +35,16 @@ namespace Addressbook.Web.Controllers
         [HttpPost]
         public ActionResult Login(LoginModel model, string returnUrl)
         {
+            var validateAndSigIn = from user in ValidateUser(model)
+                                   from signIn in SignIn(user, model.RememberMe)
+                                   select user;
+            
             //TODO: Perform validation
-
-            var isValid = true;
+            var isValid = validateAndSigIn.Succeeded;
 
             //Sign User In
             if (isValid)
             {
-                SignIn(model);
-
                 if (!string.IsNullOrEmpty(returnUrl))
                 {
                     //return Redirect(returnUrl);
@@ -49,17 +61,40 @@ namespace Addressbook.Web.Controllers
             } 
         }
 
-        private void SignIn(LoginModel model) //v6 7.29
+        private Operation<User> ValidateUser(LoginModel model) //v16 0.16
         {
-            var claims = new Claim[]{
-                new Claim(ClaimTypes.NameIdentifier, "1"),
-                new Claim(ClaimTypes.Email, model.Email),
-                new Claim(ClaimTypes.Name, model.Email),
-                new Claim(ClaimTypes.Role, "Admin")
-            }; //v5 6.35
+            return Operation.Create(() =>
+            {
+                if (ModelState.IsValid)
+                {
+                    var user = _user.Find(model.Email, model.Password);
+                    if (user == null)
+                        throw new Exception("Invalid Username");
+                    return user;
+                }
+                else
+                {
+                    var error = ModelState.Values
+                    .SelectMany(v => v.Errors)
+                    .Select(e => e.ErrorMessage)
+                    .Aggregate((ag, e) => ag + ", " + e);
 
-            var identity = new ClaimsIdentity(claims, DefaultAuthenticationTypes.ApplicationCookie); //v5 3.44 - v8 1.42
-            Authentication.SignIn(identity); //v5 3.18 
+                    throw new Exception(error);
+                }
+            });
+        }
+
+        private Operation<ClaimsIdentity> SignIn(User model, bool rememberMe) //v6 7.29 //remember me v15 8.23
+        {
+            return Operation.Create(() =>
+            {
+                var identity = _user.CreateIdentity(model, DefaultAuthenticationTypes.ApplicationCookie); //v5 3.44 - v8 1.42
+            
+                //optionally add additional claims
+                Authentication.SignIn(new AuthenticationProperties { IsPersistent = rememberMe }, identity); //v5 3.18 //V15 9.04
+
+                return identity;
+            });
         }
 
         public ActionResult LogOut()
--- README.md
+++ README.md
@@ -658,3 +658,118 @@
 								return _account.CreateUser(user).AsTask();
 							}
 						```
+						
+				- Modify some methods from AccountController
+					- Add constructor
+					- Modify Login HttpPost
+					- Modify ValidateUser
+					- Modify SignIn
+					
+						```cs
+							using Addressbook.Core.Interface.Managers;
+							using Addressbook.Core.Models;
+							using Addressbook.Web.Models;
+							using Addressbook.Web.Utils;
+							using Microsoft.AspNet.Identity;
+							using Microsoft.Owin.Security;
+							using System;
+							using System.Collections.Generic;
+							using System.Linq;
+							using System.Security.Claims;
+							using System.Web;
+							using System.Web.Mvc;
+							
+							namespace Addressbook.Web.Controllers
+							{
+								public class AccountController : Controller
+								{
+									private readonly UserManager<User, int> _user;
+									
+									public IAuthenticationManager Authentication => HttpContext.GetOwinContext().Authentication; 
+							
+									public AccountController(IAccountManager account)
+									{
+										_user = new UserManager<User, int>(new UserStore(account)); //v15 5.17
+									}
+							
+									// GET: Login
+									public ActionResult Login()
+									{
+										return View();
+									}
+							
+									[HttpPost]
+									public ActionResult Login(LoginModel model, string returnUrl)
+									{
+										var validateAndSigIn = from user in ValidateUser(model)
+															from signIn in SignIn(user, model.RememberMe)
+															select user;
+										
+										//Perform validation
+										var isValid = validateAndSigIn.Succeeded;
+							
+										//Sign User In
+										if (isValid)
+										{
+											if (!string.IsNullOrEmpty(returnUrl))
+											{
+												//return Redirect(returnUrl);
+												return RedirectToAction("Index", "Home"); //v7. 1.51
+											}
+											else
+											{
+												return RedirectToAction("Index", "Home");
+											}
+										}
+										else
+										{
+											return View(model);
+										} 
+									}
+							
+									private Operation<User> ValidateUser(LoginModel model) //v16 0.16
+									{
+										return Operation.Create(() =>
+										{
+											if (ModelState.IsValid)
+											{
+												var user = _user.Find(model.Email, model.Password);
+												if (user == null)
+													throw new Exception("Invalid Username");
+												return user;
+											}
+											else
+											{
+												var error = ModelState.Values
+												.SelectMany(v => v.Errors)
+												.Select(e => e.ErrorMessage)
+												.Aggregate((ag, e) => ag + ", " + e);
+							
+												throw new Exception(error);
+											}
+										});
+									}
+							
+									private Operation<ClaimsIdentity> SignIn(User model, bool rememberMe) //v6 7.29 //remember me v15 8.23
+									{
+										return Operation.Create(() =>
+										{
+											var identity = _user.CreateIdentity(model, DefaultAuthenticationTypes.ApplicationCookie); //v5 3.44 - v8 1.42
+										
+											//optionally add additional claims
+											Authentication.SignIn(new AuthenticationProperties { IsPersistent = rememberMe }, identity); //v5 3.18 //V15 9.04
+							
+											return identity;
+										});
+									}
+							
+									public ActionResult LogOut()
+									{
+										Authentication.SignOut();
+										return Redirect("login");
+									}
+								}
+							}
+						```
+						
+						
